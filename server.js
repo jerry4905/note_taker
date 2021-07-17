a1142e20b229ed471212560b2b4abda3e1027749
jerry <jerry4905@hotmail.com>
2021-07-16 21:58:44 -0700

    post and display working

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    noteData.push(newNote);
+    noteData.map((noteObject, i) => noteObject.id = i + 1);
+    writeJSONfile(noteData, res);
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+function writeJSONfile(jsonArray, res) {
+    fs.writeFile("./db/db.json", JSON.stringify(jsonArray), function () {
+        res.json(jsonArray);
+    });  
+}
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
